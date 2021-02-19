2df66b01c2b4b75228a7c57db672c10f757d061d
Matthieu Jacquand <matthieu.jacquand@gmail.com>
2021-02-19 17:49:15 +0100

    Final Project V1

--- ml_toolbox.py
+++ ml_toolbox.py
@@ -5,6 +5,7 @@ from sklearn.metrics import confusion_matrix
 import seaborn as sb
 import numpy as np
 import librosa
+import csv
 
 
 ##########################################
@@ -187,6 +188,87 @@ def predict_genre_knn(model, scaler, songname, encoder = None):
        'cqt9', 'tonnetz5', 'cens8', 'cens5', 'cqt4', 'cens4', 'cens11', 'cqt3',
        'tonnetz4', 'mfcc16', 'cens3', 'cqt8', 'tonnetz3', 'cens7', 'cqt7',
        'cqt2', 'cens1', 'cens9', 'cens10', 'cens6'], axis=1).values
+    
+    
+    songdata = scaler.transform(songdata)
+    songpred = model.predict(songdata)
+    
+    
+    if encoder is  None :
+        print(songname, " was identified as : " , songpred[0])
+    
+    else :
+        songpred = encoder.transform(songpred)
+        print(songname, " was identified as : " , songpred[0]) 
+        
+        
+        
+def predict_genre_lsvc(model, scaler, songname, encoder = None):
+    
+    header = 'filename chroma_stft rmse spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
+    for i in range(1, 21):
+        header += f' mfcc{i}'
+    for i in range(1, 13):
+        header += f' cqt{i}'
+    for i in range(1, 13):
+        header += f' cens{i}'
+    for i in range(1, 8):
+        header += f' cont{i}'
+    header += ' spectram_flatness'
+    for i in range(1, 7):
+        header += f' tonnetz{i}'
+    header += ' label'
+    header = header.split()
+    
+    
+    
+    file = open('dataforpredlsvc.csv', 'w', newline='')
+    with file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+    
+    y, sr = librosa.load(songname, mono=True, duration=30)
+    chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+    spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+    spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+    rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+    rmse = librosa.feature.rms(y=y)
+    zcr = librosa.feature.zero_crossing_rate(y)
+    mfcc = librosa.feature.mfcc(y=y, sr=sr)
+    #added
+    chroma_cqt = librosa.feature.chroma_cqt(y=y, sr = sr)
+    chroma_cens = librosa.feature.chroma_cens(y=y, sr = sr)
+    spec_con = librosa.feature.spectral_contrast(y=y, sr=sr)
+    spec_flat = librosa.feature.spectral_flatness(y=y)
+    tonnetz = librosa.feature.tonnetz(y = y, sr = sr)
+    to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'    
+    for e in mfcc:
+        to_append += f' {np.mean(e)}'
+    for f in chroma_cqt :
+        to_append += f' {np.mean(f)}'
+    for h in chroma_cens :
+        to_append += f' {np.mean(h)}'
+    for i in spec_con :
+        to_append += f' {np.mean(i)}'
+    for j in spec_flat :
+        to_append += f' {np.mean(j)}'
+    for k in tonnetz :
+        to_append += f' {np.mean(k)}'
+    to_append += f' {g}'
+    file = open('dataforpredlsvc.csv', 'a', newline='')
+    with file:
+        writer = csv.writer(file)
+        writer.writerow(to_append.split())
+            
+            
+    dataset_file = "dataforpredlsvc.csv"
+
+    songdata = pd.read_csv(dataset_file, sep=",")         
+   
+    songdata = songdata.drop(["label", "filename",'cqt9', 'cens11', 'cens12', 'tonnetz4', 'mfcc13', 'cens2', 'mfcc20',
+       'cqt2', 'cens4', 'mfcc10', 'tonnetz2', 'cqt12', 'tonnetz1', 'cens9',
+       'cens7', 'cqt7', 'cens5', 'tonnetz3', 'cens6', 'cqt3', 'tonnetz6',
+       'cens3', 'cens8', 'cqt10', 'tonnetz5', 'cens10', 'cens1'], axis=1).values
     
     
     songdata = scaler.transform(songdata)
